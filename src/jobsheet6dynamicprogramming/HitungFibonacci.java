da360a87757fbf611113b31e208c62b2092c5cb9
Faras <Faras@Faras-PC>
2018-09-12 13:05:31 +0700

    Initial Commit

---
+++ src/jobsheet6dynamicprogramming/HitungFibonacci.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet6dynamicprogramming;
+
+/**
+ *
+ * @author Faras
+ */
+public class HitungFibonacci {
+        private static void tampilJudul (String identitas){
+            System.out.println("Identitas: " + identitas);
+            
+            System.out.println("\nHitung Fibonacci");
+            System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        }
+        public static void main (String[] args){
+            String identitas = "Luthfarras / XR5 / 19";
+            tampilJudul(identitas);
+        }
+    }
+    
