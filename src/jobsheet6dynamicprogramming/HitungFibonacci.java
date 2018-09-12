0814aeb2f096beb3d0f675eefe67b1130acbcee9
Faras <Faras@Faras-PC>
2018-09-12 13:09:08 +0700

    Tambah Input

--- src/jobsheet6dynamicprogramming/HitungFibonacci.java
+++ src/jobsheet6dynamicprogramming/HitungFibonacci.java
@@ -5,20 +5,29 @@
  */
 package jobsheet6dynamicprogramming;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Faras
  */
 public class HitungFibonacci {
+    public static void main (String[] args){
+            String identitas = "Luthfarras / XR5 / 19";
+            tampilJudul(identitas);
+            int n = tampilInput();
+    }
         private static void tampilJudul (String identitas){
             System.out.println("Identitas: " + identitas);
-            
             System.out.println("\nHitung Fibonacci");
             System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
         }
-        public static void main (String[] args){
-            String identitas = "Luthfarras / XR5 / 19";
-            tampilJudul(identitas);
+        private static int tampilInput(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Bilangan ke-: ");
+            int n = scanner.nextInt();
+            
+            return n;
         }
     }
     
