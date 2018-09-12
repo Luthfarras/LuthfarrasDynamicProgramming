d5840d36cd1d15a4c281bd831c7fdebc9e7adf25
Faras <Faras@Faras-PC>
2018-09-12 13:15:37 +0700

    Tambah Hitung Fibonacci

--- src/jobsheet6dynamicprogramming/HitungFibonacci.java
+++ src/jobsheet6dynamicprogramming/HitungFibonacci.java
@@ -5,6 +5,7 @@
  */
 package jobsheet6dynamicprogramming;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -16,6 +17,7 @@ public class HitungFibonacci {
             String identitas = "Luthfarras / XR5 / 19";
             tampilJudul(identitas);
             int n = tampilInput();
+            BigInteger hasil = fibo(n);
     }
         private static void tampilJudul (String identitas){
             System.out.println("Identitas: " + identitas);
@@ -29,5 +31,15 @@ public class HitungFibonacci {
             
             return n;
         }
-    }
+        private static BigInteger fibo (int n){
+            BigInteger[] hasil = new BigInteger[n];
+            hasil[0] = BigInteger.ONE;
+            hasil[1] = BigInteger.ONE;
+            
+            for(int i = 2; i<n; i++){
+                hasil [i] = hasil[i-1].add(hasil[i-2]);
+            }
+            return hasil[n-1];
+        }
+}
     
