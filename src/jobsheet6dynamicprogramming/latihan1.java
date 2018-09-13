6bd7cc278b5859ce10bcf33adad648fc0b5b53c0
Faras <Faras@Faras-PC>
2018-09-13 23:23:26 +0700

    Input Jumlah

--- src/jobsheet6dynamicprogramming/latihan1.java
+++ src/jobsheet6dynamicprogramming/latihan1.java
@@ -14,6 +14,14 @@ public class latihan1 {
         String identitas = "Luthfarras / XR5 / 19";
         int uang = 421300;
         int pecahan[] = new int[]{5000, 2000, 1000, 500};
+        int jumlah;
+        for(int i = 0; i < pecahan.length; i++){
+            if(uang >= pecahan[i]){
+                jumlah = uang/pecahan[i];
+                uang = uang%pecahan[i];
+        
+                System.out.println("Pecahan " +pecahan[i]+ " sebanyak " +jumlah+ " lembar");
+            }
     }
-    
+    }
 }
