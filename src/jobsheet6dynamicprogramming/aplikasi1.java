fba1f54690999fc36482aa307e52b09bcd653c8a
Faras <Faras@Faras-PC>
2018-09-13 20:44:28 +0700

    Tambah Jumlah

--- src/jobsheet6dynamicprogramming/aplikasi1.java
+++ src/jobsheet6dynamicprogramming/aplikasi1.java
@@ -17,15 +17,28 @@ public class aplikasi1 {
         tampilJudul(identitas);
         int pecahan[] = new int[]{5000, 2000, 1000, 500};
         int n = input();
+        jumlah();
     }
     private static void tampilJudul (String identitas){
             System.out.println("Identitas: " + identitas);
         }
     private static int input(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Bilangan ke-: ");
+        System.out.print("Masukkan jumlah uang: ");
         int n = scanner.nextInt();
             
         return n;
     }
-}
+    private static int jumlah(){
+        int uang;
+        int pecahan[] = new int[]{5000, 2000, 1000, 500};
+        int hasil;
+        
+        for(int i = 0; i < pecahan.length; i++){
+            if(uang >= pecahan[i]){
+                hasil = uang/pecahan[i];
+                uang = uang%pecahan[i];
+    }
+        
+    }
+        
