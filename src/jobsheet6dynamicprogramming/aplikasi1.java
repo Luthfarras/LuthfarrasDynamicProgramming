a1fb7f8aded1b697c59aeab0ef01710a6d47fd04
Faras <Faras@Faras-PC>
2018-09-13 19:52:21 +0700

    Tambah Input

--- src/jobsheet6dynamicprogramming/aplikasi1.java
+++ src/jobsheet6dynamicprogramming/aplikasi1.java
@@ -5,6 +5,8 @@
  */
 package jobsheet6dynamicprogramming;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Faras
@@ -13,8 +15,17 @@ public class aplikasi1 {
     public static void main(String[] args){
         String identitas = "Luthfarras / XR5 / 19";
         tampilJudul(identitas);
+        int pecahan[] = new int[]{5000, 2000, 1000, 500};
+        int n = input();
     }
     private static void tampilJudul (String identitas){
             System.out.println("Identitas: " + identitas);
         }
+    private static int input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bilangan ke-: ");
+        int n = scanner.nextInt();
+            
+        return n;
+    }
 }
