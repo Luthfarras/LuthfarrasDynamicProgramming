1bf21f443a445845acbb0925a388f9ef35c46ed5
Faras <Faras@Faras-PC>
2018-09-13 19:43:35 +0700

    Initial Commit

---
+++ src/jobsheet6dynamicprogramming/aplikasi1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet6dynamicprogramming;
+
+/**
+ *
+ * @author Faras
+ */
+public class aplikasi1 {
+    public static void main(String[] args){
+        String identitas = "Luthfarras / XR5 / 19";
+        tampilJudul(identitas);
+    }
+    private static void tampilJudul (String identitas){
+            System.out.println("Identitas: " + identitas);
+        }
+}
